aa21437f8f2ee65f8c1a215d3b1dab184004edc8
tallman <vedunaik@gmail.com>
2023-12-27 10:45:02 +0530

    refactored skills code

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -1,26 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
 import { animationProps } from '../variants';
-import {
-    SiNextdotjs,
-    SiJavascript,
-    SiTailwindcss,
-    SiTypescript,
-    SiReact,
-    SiRedux,
-    SiHtml5,
-    SiCss3,
-} from 'react-icons/si';
-const Icons = [
-    { id: '1', name: 'Html', icon: <SiHtml5 size={50} /> },
-    { id: '2', name: 'Css', icon: <SiCss3 size={50} /> },
-    { id: '3', name: 'Tailwind css', icon: <SiTailwindcss size={50} /> },
-    { id: '4', name: 'Javascript', icon: <SiJavascript size={50} /> },
-    { id: '5', name: 'Typescript', icon: <SiTypescript size={50} /> },
-    { id: '6', name: 'React.js', icon: <SiReact size={50} /> },
-    { id: '7', name: 'Redux', icon: <SiRedux size={50} /> },
-    { id: '8', name: 'Next.js', icon: <SiNextdotjs size={50} /> },
-];
+import { Icons } from '@/lib/ReactIcons';
 
 const Skills = () => {
     return (
---
+++ lib/ReactIcons.tsx
@@ -0,0 +1,32 @@
+import {
+    SiNextdotjs,
+    SiJavascript,
+    SiTailwindcss,
+    SiTypescript,
+    SiReact,
+    SiRedux,
+    SiHtml5,
+    SiCss3
+} from 'react-icons/si';
+
+export const Icons = [
+    { id: '1', name: 'Html', icon: <SiHtml5 size={50} /> },
+    { id: '2', name: 'Css', icon: <SiCss3 size={50} /> },
+    { id: '3', name: 'Tailwind css', icon: <SiTailwindcss size={50} /> },
+    { id: '4', name: 'Javascript', icon: <SiJavascript size={50} /> },
+    { id: '5', name: 'Typescript', icon: <SiTypescript size={50} /> },
+    { id: '6', name: 'React.js', icon: <SiReact size={50} /> },
+    { id: '7', name: 'Redux', icon: <SiRedux size={50} /> },
+    { id: '8', name: 'Next.js', icon: <SiNextdotjs size={50} /> }
+];
+
+export {
+    SiNextdotjs,
+    SiJavascript,
+    SiTailwindcss,
+    SiTypescript,
+    SiReact,
+    SiRedux,
+    SiHtml5,
+    SiCss3
+};
