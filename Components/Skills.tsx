9390b3ea109566a43227e1dabe77388ac4333059
tallman <vedunaik@gmail.com>
2024-01-13 08:45:36 +0530

    -transparent&filter from bg

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -11,42 +11,42 @@ const Skills = () => {
                 </h2>
                 <div className='grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xs:gap-[30px] lg:gap-[20px] xl:gap-[30px] mt-[50px]'>
 
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-orange-500'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-orange-500'>
                         <p className='text-white text-6xl font-bold align-middle h-full flex justify-center items-center'>
                             HTML5
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full bg-[#214ce5]'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full bg-[#214ce5]'>
                         <p className='text-white text-6xl font-bold align-middle h-full flex justify-center items-center'>
                             CSS3
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-[#38bdf8]'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-[#38bdf8]'>
                         <p className='text-white text-6xl lg:text-5xl text-center font-bold align-middle h-full flex justify-center items-center'>
                             Tailwind css
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-yellow-400'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-yellow-400'>
                         <p className='text-white text-6xl lg:text-5xl font-bold align-middle h-full flex justify-center items-center'>
                             Javascript
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-[#2f74c0]'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-[#2f74c0]'>
                         <p className='text-white text-6xl lg:text-5xl font-bold align-middle h-full flex justify-center items-center'>
                             Typescript
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a] h-[200px] w-full mx-auto bg-black'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a] h-[200px] w-full mx-auto bg-black'>
                         <p className='text-[#00cff4] text-6xl font-bold align-middle h-full flex justify-center items-center'>
                             React.js
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a] mx-auto xl:col-span-2 h-[200px] w-full bg-purple-700'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto xl:col-span-2 h-[200px] w-full bg-purple-700'>
                         <p className='text-white text-6xl  font-bold flex justify-center items-center h-full'>
                             Redux
                         </p>
                     </div>
-                    <div className='rounded-xl bg-transparent backdrop-blur-3xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full  lg:col-span-2 xl:col-span-2 bg-white'>
+                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full  lg:col-span-2 xl:col-span-2 bg-white'>
                         <p className='text-black text-6xl font-bold h-full flex justify-center items-center'>
                             Next.js
                         </p>
