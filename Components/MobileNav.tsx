5d6b606babff54ead07308af80bbbc16248e5337
tallman <vedunaik@gmail.com>
2024-03-13 15:28:44 +0530

    created navLinks {} & removed navMenu onclick

--- Components/MobileNav.tsx
+++ Components/MobileNav.tsx
@@ -1,70 +1,41 @@
 import React from 'react'
 import { Link } from 'react-scroll';
+import { NavLinks } from '@/data/NavLinks';
 
 type Props = {
     show: boolean;
     setShow: (show: boolean) => void;
 };
 
+
+
 const MobileNav = ({ show, setShow }: Props) => {
+      const handleClickNav = () => {
+        setShow(false)
+      }
+
+       
   return (
-                    <div
-                        className={
-                            show
-                                ? 'md:hidden xs:p-4'
-                                : 'px-2 sm:px-6  md:hidden'
-                        }>
-                        <div
-                            className='flex flex-col items-center sm:items-center justify-center gap-6 p-1'
-                            onClick={() => setShow(!show)}>
-                            <Link
-                                href='home'
-                                to='home'
-                                smooth={true}
-                                spy={true}
-                                offset={-200}
-                                className='cursor-pointer active text-white'>
-                                Home
-                            </Link>
-                            <Link
-                                href='skills'
-                                to='skills'
-                                smooth={true}
-                                spy={true}
-                                offset={-20}
-                                className='cursor-pointer active text-white'>
-                                Skills
-                            </Link>
-                            <Link
-                                href='about'
-                                to='about'
-                                smooth={true}
-                                spy={true}
-                                className='cursor-pointer  active text-white'>
-                                About
-                            </Link>
-                            <Link
-                                href='work'
-                                to='work'
-                                smooth={true}
-                                spy={true}
-                                offset={5}
-                                className='cursor-pointer  active text-white'>
-                                Projects
-                            </Link>
-                            <Link
-                                href='contact'
-                                to='contact'
-                                smooth={true}
-                                spy={true}
-                                offset={-10}
-                                className='cursor-pointer  active text-white'>
-                                Contact
-                            </Link>
-                        </div>
-                    </div>
-               
-  )
+      <div className={show ? 'md:hidden xs:p-4' : 'px-2 sm:px-6  md:hidden'}>
+          <div
+              className='flex flex-col items-center sm:items-center justify-center gap-6 p-1'
+              onClick={() => setShow(!show)}>
+              {NavLinks.nav.map((res) => (
+                  <Link
+                      onClick={handleClickNav}
+                      key={res.href}
+                      href={res.href}
+                      to={res.to}
+                      smooth={res.smooth}
+                      spy={res.spy}
+                      offset={res.offset}
+                      className='cursor-pointer active text-white capitalize'>
+                      {res.href}
+                  </Link>
+              ))}
+          </div>
+      </div>
+  );
 }
 
 export default MobileNav
---
+++ data/NavLinks.ts
@@ -0,0 +1,50 @@
+type Nav = {
+    href: string;
+    to: string;
+    smooth: boolean;
+    spy: boolean;
+    offset?: number;
+};
+
+interface Props {
+    nav: Nav[];
+}
+
+export const NavLinks: Props = {
+    nav: [
+        {
+            href: 'home',
+            to: 'home',
+            smooth: true,
+            spy: true,
+            offset: -200
+        },
+        {
+            href: 'skills',
+            to: 'skills',
+            smooth: true,
+            spy: true,
+            offset: -20
+        },
+        {
+            href: 'about',
+            to: 'about',
+            smooth: true,
+            spy: true
+        },
+        {
+            href: 'work',
+            to: 'work',
+            smooth: true,
+            spy: true,
+            offset: 5
+        },
+        {
+            href: 'contact',
+            to: 'contact',
+            smooth: true,
+            spy: true,
+            offset: -10
+        }
+    ]
+};
