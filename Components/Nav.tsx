5fe6e2649809c1fd2c3fae66a4fdec5155204bac
tallman <vedunaik@gmail.com>
2024-03-15 16:37:24 +0530

    removed old humburgger

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Link } from 'react-scroll';
+import MenuSvg from './MenuSvg';
 
 type Props = {
     show: boolean;
@@ -63,19 +64,9 @@ const Navbar = ({ show, setShow }: Props) => {
                 </div>
             </div>
             <div
-                className='flex flex-col gap-1 md:hidden absolute top-5 right-5  '
+                className='flex flex-col gap-1 md:hidden absolute top-[26px] right-5  '
                 onClick={() => setShow(!show)}>
-                <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    stroke-width='0'
-                    viewBox='0 0 512 512'
-                    color='white'
-                    height='25px'
-                    width='25px'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z'></path>
-                </svg>
+                <MenuSvg show={show}/>
             </div>
         </nav>
     );
