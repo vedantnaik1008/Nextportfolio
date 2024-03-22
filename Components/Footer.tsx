902dd2ebaa8186ee621a55328ece535817ef30ba
tallman <vedunaik@gmail.com>
2024-03-22 21:18:05 +0530

    +footer

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,7 +3,7 @@ import  Link  from "next/link";
 
 const Footer = () => {
     return (
-        <div className='py-5 border-t-[#5a5a5a] border-t-2 mx-auto w-[90%]'>
+        <footer className='py-5 border-t-[#5a5a5a] border-t-2 mx-auto w-[90%]'>
             <div className=' flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col py-5'>
                 <p className='caption text-white lg:block'>
                     © {new Date().getFullYear()}. All rights reserved.
@@ -16,7 +16,7 @@ const Footer = () => {
                     ))}
                 </ul>
             </div>
-        </div>
+        </footer>
     );
 };
 
