2057347480a123dcaecceb89f16cfe6368ca8693
tallman <vedunaik@gmail.com>
2024-05-18 17:30:07 +0530

    removed unused code

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -13,49 +13,6 @@ const Skills = () => {
                 <h2 className={`${dancingScript.className} h2 leading-tight text-center text-clip text-transparent bg-clip-text bg-gradient-to-r from-[#019ae9] to-[#00dfd8]`}>
                     My Skills
                 </h2>
-                {/* <div className='grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xs:gap-[30px] lg:gap-[20px] xl:gap-[30px] mt-[50px]'>
-
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-orange-500'>
-                        <p className='text-white text-6xl font-bold align-middle h-full flex justify-center items-center'>
-                            HTML5
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full bg-[#214ce5]'>
-                        <p className='text-white text-6xl font-bold align-middle h-full flex justify-center items-center'>
-                            CSS3
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-[#38bdf8]'>
-                        <p className='text-white text-6xl lg:text-5xl text-center font-bold align-middle h-full flex justify-center items-center'>
-                            Tailwind css
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a]  mx-auto h-[200px] w-full bg-yellow-400'>
-                        <p className='text-white text-6xl lg:text-5xl font-bold align-middle h-full flex justify-center items-center'>
-                            Javascript
-                        </p>
-                    </div>
-                    <div className='rounded-xl overflow-hidden border-[2px] border-[#5a5a5a]  mx-auto xl:col-span-2 h-[200px] w-full bg-[#2f74c0]'>
-                        <p className='text-white text-6xl lg:text-5xl font-bold align-middle h-full flex justify-center items-center'>
-                            Typescript
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a] h-[200px] w-full mx-auto bg-black'>
-                        <p className='text-[#00cff4] text-6xl font-bold align-middle h-full flex justify-center items-center'>
-                            React.js
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto xl:col-span-2 h-[200px] w-full bg-purple-700'>
-                        <p className='text-white text-6xl  font-bold flex justify-center items-center h-full'>
-                            Redux
-                        </p>
-                    </div>
-                    <div className='rounded-xl border-[2px] border-[#5a5a5a] mx-auto h-[200px] w-full  lg:col-span-2 xl:col-span-2 bg-white'>
-                        <p className='text-black text-6xl font-bold h-full flex justify-center items-center'>
-                            Next.js
-                        </p>
-                    </div>
-                </div> */}
                 <div className='mt-12'>
                     <div className='my-4'>
                         <h3 className='h3 leading-tight text-center text-white mb-8'>
